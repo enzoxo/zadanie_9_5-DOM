9b8130054bf22dd8ca58065d5f20331f45a1822e
Michal Bandyszak <michalbandyszak@gmail.com>
2018-04-22 16:46:02 +0200

    Change script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var ilosc_Elem = document.getElemetsByClassName("button").innerText;
-for (var i = 0; i <= ilosc_Elem.length; i++){
-	console.log(ilosc_Elem);
+var elemButtonClass = document.getElemetsByClassName("button");
+var ilosc_Elem = elemButtonClass.length;
+var textContent ="";
+for (var i = 0; i <= ilosc_Elem i++){
+	textContent = elemButtonClass[i];
+	console.log(textContent.innerText);
 }
